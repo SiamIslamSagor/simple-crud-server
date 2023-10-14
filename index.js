0e63ee0da6215169d63bdfe8d5eb79257e30bb7c
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-15 00:27:06 +0600

    find a single user data

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ async function run() {
       res.send(result);
     });
 
+    // to find one
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const user = await userCollection.findOne(query);
+      res.send(user);
+    });
+
     // to create data
     app.post("/users", async (req, res) => {
       const user = req.body;
